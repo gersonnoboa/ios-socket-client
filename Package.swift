e637be8097f1b08a99281193780f5b69fcb1cb4f
Gerson Noboa <gerson.noboa@glia.com>
2021-02-16 15:37:40 +0200

    Add SPM full support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SocketIO"
+    name: "SocketIO",
+    products: [
+        .library(name: "SocketIO", targets: ["SocketIO"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream", .upToNextMinor(from: "4.0.0")),
+    ],
+    targets: [
+        .target(name: "SocketIO", dependencies: ["Starscream"]),
+        .testTarget(name: "TestSocketIO", dependencies: ["SocketIO"]),
+    ]
 )
